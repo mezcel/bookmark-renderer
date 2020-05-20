e6960507ba132a2db16de64b7d05a9866b3126c0
mezcel <mezcel@hotmail.com>
2020-05-20 00:21:57 -0400

    added an online status flag

--- preload.js
+++ preload.js
@@ -1,17 +1,27 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector)
+		if (element) element.innerText = text
+	}
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+	for (const type of ['chrome', 'node', 'electron']) {
+		replaceText(`${type}-version`, process.versions[type])
+	}
 
-  replaceText(`platform-arch`, process.platform)
-  replaceText(`cd`, process.cwd() )
+	replaceText(`platform-arch`, process.platform)
+	replaceText(`cd`, process.cwd() )
+
+	require('dns').resolve('www.google.com', function(err) {
+		if (err) {
+			//console.log("No connection"); 
+			replaceText(`isOnline`, "offline" );
+		} else {
+			//console.log("Connected");
+			replaceText(`isOnline`, "online" );
+		}
+	});
 
 });
 
