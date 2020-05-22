0025463076a1fab399d4d854ad4fc3ec64ce66a0
mezcel <mezcel@hotmail.com>
2020-05-21 23:49:39 -0400

    updated script

--- powershell/install-nodejs-win10.ps1
+++ powershell/install-nodejs-win10.ps1
@@ -8,32 +8,21 @@ $arch = $env:PROCESSOR_ARCHITECTURE
 $length = $arch.length
 $archNo = $arch.substring($length -2)
 
-if ( $archNo == "64" ) {
+if ( $archNo = "64" ) {
 	$url = "https://nodejs.org/dist/v12.16.3/node-v12.16.3-x64.msi"
 } else {
 	$url = "https://nodejs.org/dist/v12.16.3/node-v12.16.3-x86.msi"
 }
 
-write-host "Downloading NodeJS ...`n`t$url"
+write-host "Downloading NodeJS ...`n`t$url" -ForegroundColor Magenta
 $outputFile = Split-Path $url -leaf
 
 $output = "C:\Users\$env:UserName\Downloads\$outputFile"
 $output = "$env:UserProfile\Downloads\$outputFile"
 
+## Commence Download
 Invoke-WebRequest -Uri $url -OutFile $output
 
-$yn = Read-Host "`nDo you want to install $outputFile ? [ y/N ]"
-
-if ( $yn = "y" ) {
-	## if file exists, then do the do
-	if ( Test-Path -Path $output -PathType Leaf ) {
-		write-host "`ninstalling $output ...`n"
-		Start-Process -FilePath $output
-	} else {
-		downloadNodejs
-		Start-Process -FilePath $output
-	}
-}
-write-host "`nDone."
-write-host "Press any key to continue..."
+write-host "`nDone.`n`tCheck $output if the executable did not automatically run." -ForegroundColor Green
+write-host "Press any key to continue..." -ForegroundColor Green
 [void][System.Console]::ReadKey($true)
