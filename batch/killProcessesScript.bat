c81f3e913788286f45e92331f9da30d43536bfb3
mezcel <mezcel@hotmail.com>
2020-05-22 07:14:56 -0400

    kill steam

--- batch/killProcessesScript.bat
+++ batch/killProcessesScript.bat
@@ -7,6 +7,8 @@
 :::: Kill steam
 set appExe="C:\Program Files (x86)\Steam\Steam.exe"
 taskkill /f /im %appExe% 2> nul
+set appExe="steam.exe"
+taskkill /f /im %appExe% 2> nul
 
 :::: Kill gamebar
 set appExe="GameBar.exe"
@@ -14,6 +16,10 @@ taskkill /f /im %appExe% 2> nul
 
 :::: Kill ms-edge
 set appExe="MicrosoftEdge.exe"
+taskkill /f /im %appExe% /t 2> nul
+set appExe="MicrosoftEdgeCP.exe"
+taskkill /f /im %appExe% /t 2> nul
+set appExe="browser_broker.exe"
 taskkill /f /im %appExe% /t 2> nul
 
 :: Kill Cortana
