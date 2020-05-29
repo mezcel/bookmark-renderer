ee4657ca288b5aac765755af5231e2403991182d
mezcel <mezcel@hotmail.com>
2020-05-29 07:48:00 -0400

    update comments

--- Powershell/build-package-app.ps1
+++ Powershell/build-package-app.ps1
@@ -1,11 +1,42 @@
 <#
     app-shortcut-links.ps1
-    remove previous build and package a new build
-    desktop shortcut and startup shortcut
+
+    About:
+        remove previous build and package a new build
+        desktop shortcut and startup shortcut
+
+    Enable this script in Powershell:
+        Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 #>
+
+
+$scriptParentDir = split-path -parent $MyInvocation.MyCommand.Definition
+$scriptName = $MyInvocation.MyCommand.Name
+
+function greetings() {
+
+    Write-Host "//////////////////////////////////////////////////////////////////////////////"
+    Write-Host "# $scriptName"
+    Write-Host "# "
+    Write-Host "# About:"
+    Write-Host "#`tRemove previous build and package a new build."
+    Write-Host "#`tMake a desktop shortcut and a startup shortcut."
+    Write-Host "# "
+    Write-Host "# By:"
+    Write-Host "#`tMezcel ( https://github.com/mezcel/bookmark-renderer )"
+    Write-Host "# "
+    Write-Host "# Source:"
+    Write-Host "#`thttps://github.com/mezcel/bookmark-renderer/blob/master/Powershell/build-package-app.ps1"
+    Write-Host "# "
+    Write-Host "# Script Path:"
+    Write-Host "#`t$scriptParentDir\$scriptName"
+    Write-Host "//////////////////////////////////////////////////////////////////////////////"
+    Start-Sleep 3
+}
 
 function killRunningProcess() {
     ## kill any running  processes
+
     Write-Host "kill any running bookmark-renderer* processes ..." -ForegroundColor Cyan
     Stop-Process -Name "bookmark-renderer*"
 
@@ -44,6 +75,7 @@ function buildNewPackage() {
 
     Write-Host "Build a new package ..." -ForegroundColor Cyan
     if ( -Not ( Test-Path $packagePath2 ) -and -Not ( Test-Path $packagePath1 ) ) {
+        ## electron-packager . --force
         electron-packager .
         Write-Host "Pause to allow built package to settle in ..." -ForegroundColor DarkYellow
         Start-Sleep 3
@@ -52,6 +84,8 @@ function buildNewPackage() {
 }
 
 function set-shortcut( [string]$ShortcutFile, [string]$WorkingDir, [string]$TargetFile ) {
+    # Make a shortcut file .lnk
+
     $WScriptShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
     $Shortcut.TargetPath = $TargetFile
@@ -59,6 +93,8 @@ function set-shortcut( [string]$ShortcutFile, [string]$WorkingDir, [string]$Targ
     $Shortcut.Save()
 }
 function createShortcutLinks() {
+    # Make a startup link and a desktop shortcut
+    # The start menu shortcut does not work, but it is made as well
 
     $packagePath1 = "bookmark-renderer-win32-x64"
     $packagePath2 = "bookmark-renderer-win32-ia32"
@@ -100,6 +136,8 @@ function createShortcutLinks() {
 }
 
 function getNpms() {
+    # Install npm packages
+    # N/a
 
     Write-Host "Installing packages listed in package.json ..." -ForegroundColor Cyan
     npm install
@@ -113,7 +151,9 @@ function getNpms() {
 }
 
 function main() {
-    ## Install npm package
+    greetings
+
+    ## Install npm packages
     #getNpms
 
     killRunningProcess
@@ -122,7 +162,7 @@ function main() {
     createShortcutLinks
 }
 
-<# RUN #>
+<# ############# RUN ############# #>
 
 main
 
