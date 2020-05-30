4b37a073bca2085bf342ac0a0c3b00fe449a4767
mezcel <mezcel@hotmail.com>
2020-05-30 16:23:48 -0400

    updated tray menu

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 /*  main.js */
 
 // Modules to control application life and create native browser window
+
 const { app, BrowserWindow, shell, Menu, Tray } = require( 'electron' );
 
-const path = require( 'path' );
-const url  = require( 'url' );
-const nativeImage = require('electron').nativeImage;
+const { execFile, spawn } = require( 'child_process' );
+
+const path        = require( 'path' );
+const url         = require( 'url' );
+const nativeImage = require( 'electron' ).nativeImage;
 
 // Global theme css variable
 global.GlobalTheme = {
@@ -14,11 +17,39 @@ global.GlobalTheme = {
 
 /* -------------------------------------------------------------------------- */
 
+function launchBatScript( scriptPath ) {
+    const bat = spawn( 'cmd.exe', [ '/c', scriptPath ]);
+
+    bat.stdout.on( 'data', ( data ) => {
+        console.log( data.toString() );
+    });
+
+    bat.stderr.on( 'data', ( data ) => {
+        console.log( "\n script path = " + scriptPath + " \n", data.toString() );
+    });
+
+    bat.on( 'exit', ( code ) => {
+        console.log( `Child exited with code ${code}` );
+    });
+}
+
 function createWindow () {
     Menu.setApplicationMenu( null ); // null menubar
 
-    const faviconPath = path.join( __dirname, 'view/img/electron_favicon.ico' );
-    const favicon     = nativeImage.createFromPath( faviconPath );
+    const faviconPath = path.join( __dirname, 'view/img/electron_favicon.ico' ),
+          favicon     = nativeImage.createFromPath( faviconPath ).resize({width:16});
+
+    const githubiconPath = path.join( __dirname, 'view/img/github.ico' ),
+          githubicon     = nativeImage.createFromPath( githubiconPath ).resize({width:16});
+
+    const mdiconPath = path.join( __dirname, 'view/img/md.ico' ),
+          mdicon     = nativeImage.createFromPath( mdiconPath ).resize({width:16});
+
+    const psiconPath = path.join( __dirname, 'view/img/ps.ico' ),
+          psicon     = nativeImage.createFromPath( psiconPath ).resize({width:16});
+
+    const closeiconPath = path.join( __dirname, 'view/img/close.ico' ),
+          closeicon     = nativeImage.createFromPath( closeiconPath ).resize({width:16});
 
     // Create the browser window.
     const mainWindow = new BrowserWindow({
@@ -46,7 +77,90 @@ function createWindow () {
             label: 'Git: ' + app.name,
             click () {
                 shell.openExternal( 'https://github.com/mezcel/bookmark-renderer' );
-            }
+            },
+            icon: githubicon
+        },
+        { type: 'separator' },
+        {
+            label: 'Bookmarks',
+            'submenu': [
+                {
+                    label: 'Bookmarks Menu',
+                    click () {
+                        mainWindow.loadURL( path.join( __dirname, 'view/html/bookmarks.html' ) );
+                        mainWindow.restore();
+                    },
+                    icon: favicon
+                },
+                { type: 'separator' },
+                {
+                    label: 'Github ',
+                    click () {
+                        shell.openExternal( 'https://github.com/mezcel' );
+                    }
+                }, {
+                    label: 'Gist',
+                    click () {
+                        shell.openExternal( 'https://gist.github.com/mezcel' );
+                    }
+                }, {
+                    label: 'Google',
+                    click () {
+                        shell.openExternal( 'https://google.com' );
+                    }
+                }, {
+                    label: 'Youtube',
+                    click () {
+                        shell.openExternal( 'https://www.youtube.com/channel/UCpNZc5SjrUzVwGgI9PDMHxg' );
+                    }
+                }, {
+                    label: 'Soundcloud',
+                    click () {
+                        shell.openExternal( 'https://soundcloud.com/mezcel' );
+                    }
+                }
+            ],
+            icon: favicon
+        },{
+            label: 'Scripts',
+            'submenu': [
+                {
+                    'label': 'Scripts Menu',
+                    click() {
+                        mainWindow.loadURL( path.join( __dirname, 'view/html/scripts.html' ) );
+                        mainWindow.restore();
+                    },
+                    icon: favicon
+                },
+                { type: 'separator' },
+                {
+                    'label': 'Connection Manager',
+                    click() {
+                        console.log("Connection Manager");
+                        var scriptPath = path.join( __dirname, "Batch/connmanScript.bat" );
+                        launchBatScript( scriptPath );
+                    },
+                    icon: psicon
+                },
+                {
+                    'label': 'Kill Misc. Processes',
+                    click() {
+                        console.log("Kill Misc. Processes");
+                        var scriptPath = path.join( __dirname, "Batch/killProcessesScript.bat" );
+                        launchBatScript( scriptPath );
+                    },
+                    icon: psicon
+                },
+                {
+                    'label': 'Shutdown',
+                    click() {
+                        var scriptPath = path.join( __dirname, "Batch/shutdownScript.bat" );
+                        launchBatScript( scriptPath );
+                    },
+                    icon: psicon
+                }
+            ],
+            icon: favicon
         },
         { type: 'separator' },
         {
@@ -63,14 +177,16 @@ function createWindow () {
                 }).loadURL(
                     path.join( __dirname, 'view/html/markdown.html' )
                 );
-            }
+            },
+            icon: mdicon
         },
         { type: 'separator' },
         {
-            label: 'Exit App',
+            label: 'Close App',
             click () {
                 mainWindow.close();
-            }
+            },
+            icon: closeicon
         }
     ]);
 
@@ -84,6 +200,7 @@ function createWindow () {
 
     // start bar tray
     const trayIcon = favicon.resize({ width: 16, height: 16 });
+
     mainWindow.tray = new Tray( trayIcon );
     mainWindow.tray.setToolTip( 'kiosk' );
     mainWindow.tray.setContextMenu( contextMenu );
@@ -92,9 +209,7 @@ function createWindow () {
     // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-    createWindow();
-});
+/* -------------------------------------------------------------------------- */
+
+// Run Main App
+app.whenReady().then( () => { createWindow(); } );
